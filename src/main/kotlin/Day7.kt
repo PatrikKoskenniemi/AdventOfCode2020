ad578988071a50ad0ffa975ea4712b8c811d2c58
Patrik Koskenniemi <patrik.koskenniemi@gmail.com>
2021-01-16 20:10:55 +0100

    Day 7, star 2

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -1,12 +1,13 @@
-class Day7() {
+import java.util.stream.Collectors
 
-    private var bags = mutableMapOf<String, Boolean>()
-    private val EMPTY_BAG = "no other bags"
+class Day7 {
+
+    private val EMPTY_BAG = "no other bag"
     private val SHINY_BAG = "shiny gold bag"
     private val SHINY_BAGS = "shiny gold bags"
 
     fun solveFirstStar(input: MutableList<String>): Int {
-        bags = input.associateBy({ it }, { false }).toMutableMap()
+        val bags = input.associateBy({ it }, { false }).toMutableMap()
 
         val foundRows = ArrayList<String>()
         for ((row, processed) in bags) {
@@ -15,20 +16,24 @@ class Day7() {
             } else if (!processed and row.contains(SHINY_BAG).or(row.contains(SHINY_BAGS))) {
                 foundRows.add(row)
                 bags[row] = true
-                foundRows.addAll(recursiveLookup(row.split("contain")[0], foundRows))
+                foundRows.addAll(recursiveLookupStar1(bags, row.split("contain")[0], foundRows))
             }
         }
         return foundRows.distinct().size
     }
 
-    private fun recursiveLookup(bagKey: String, foundRows: ArrayList<String>): List<String> {
+    private fun recursiveLookupStar1(
+        bags: MutableMap<String, Boolean>,
+        bagKey: String,
+        foundRows: ArrayList<String>
+    ): List<String> {
         var foundMatch = false
         for ((row, processed) in bags) {
             if (!processed and row.contains(bagKey.trim().replace("bags", "bag"))) {
                 foundMatch = true
                 foundRows.add(row)
                 bags[row] = true
-                recursiveLookup(row.split("contain")[0], foundRows)
+                recursiveLookupStar1(bags, row.split("contain")[0], foundRows)
             }
         }
         return if (!foundMatch) {
@@ -36,6 +41,48 @@ class Day7() {
         } else {
             emptyList()
         }
+    }
 
+    fun solveSecondStar(input: MutableList<String>): Int {
+        val bags = HashMap<String, List<String>>()
+        for (bagRow in input) {
+            val bagRule = bagRow.split("contain", ",").stream()
+                .map { it.removeSuffix(".") }
+                .map { it.replace("bags", "bag") }
+                .map { it.trim() }
+                .collect(Collectors.toList())
+
+            val bagRuleDescription = bagRule.subList(1, bagRule.size)
+            bags[bagRule[0]] = bagRuleDescription
+
+            val startKeys = ArrayList<String>()
+            startKeys.addAll(
+                bagRuleDescription.stream()
+                    .filter { it.contains(SHINY_BAG) }
+                    .collect(Collectors.toList()))
+        }
+        val recursiveLookup2 = recursiveLookupStar2(bags, "shiny gold bag", 1, 0)
+        return recursiveLookup2.minus(1)
+    }
+
+    private fun recursiveLookupStar2(
+        bags: HashMap<String, List<String>>,
+        bagKey: String,
+        multiplier: Int,
+        sum: Int
+    ): Int {
+        var localSum = 0
+        val bagRuleDescriptions = bags[bagKey]
+        if (bagRuleDescriptions != null) {
+            for (bagRuleDescription in bagRuleDescriptions) {
+                if (bagRuleDescription == EMPTY_BAG) {
+                    return multiplier
+                }
+                val multiplierAndBagKey = bagRuleDescription.split(" ", limit = 2)
+                val newMultiplier = multiplier.times(multiplierAndBagKey[0].toInt())
+                localSum = localSum.plus(recursiveLookupStar2(bags, multiplierAndBagKey[1], newMultiplier, sum))
+            }
+        }
+        return sum.plus(localSum).plus(multiplier)
     }
 }
--- src/test/kotlin/Day7Test.kt
+++ src/test/kotlin/Day7Test.kt
@@ -30,5 +30,33 @@ class Day7Test {
         val input = inputReader.readDaySevenAsListOfString("input_day7.txt").toMutableList()
         val solution = day7Solver.solveFirstStar(input)
         println("Solution:$solution")
+        //Solution:272
+    }
+
+    @Test
+    fun exampleTestStar2() {
+        val input = """
+            light red bags contain 1 bright white bag, 2 muted yellow bags.
+            dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+            bright white bags contain 1 shiny gold bag.
+            muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+            shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+            dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+            vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+            faded blue bags contain no other bags.
+            dotted black bags contain no other bags.
+            """.trimIndent()
+            .split("\n").toMutableList()
+
+        val solution = day7Solver.solveSecondStar(input)
+        Assert.assertEquals((1 + 1 * 7) + (2 + 2 * 11), solution)
+    }
+
+    @Test
+    fun realTestStar2() {
+        val input = inputReader.readDaySevenAsListOfString("input_day7.txt").toMutableList()
+        val solution = day7Solver.solveSecondStar(input)
+        println("Solution:$solution")
+        //Solution:172246
     }
 }
